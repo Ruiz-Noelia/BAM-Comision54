c4b5d740ee8d63e494690463ce9594f6ba993940
Ruiz-Noelia <sagitrinidad@gmail.com>
2023-10-17 18:44:59 -0300

    Clase 9 (17/10): Hola Mundo en JS

---
+++ clase9/script.js
@@ -0,0 +1,12 @@
+//console.log("Hola, este es un mensaje secreto para quienes saben usar la consola");
+
+document.write("Hola Mundo desde JS");
+
+
+
+// console.log("El número 120 es de tipo: " + typeof(120.90));
+// console.log(typeof("hola a todos"));
+
+// console.log("El valor true es de tipo: " + typeof(true));
+// console.log(typeof(false));
+
